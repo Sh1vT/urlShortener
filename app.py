e41139a8d3584d24b2c7fd19b9f769f5fb4c1b29
ppkekw <accplaceholder311@gmail.com>
2025-02-04 20:45:19 +0530

    port fix trials

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def redirect_to_long(short_url):
     return jsonify({'error': 'URL Not Found'}), 404
 
 if __name__ == '__main__':
-    port=int(os.environ.get('PORT', 4000))
+    port=int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
